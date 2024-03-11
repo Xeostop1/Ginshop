e76f159f98dbcd468f65dc0facce6dc9f06c08d6
Xeostop1 <stop1@daum.net>
2024-03-11 19:04:26 +0900

    add :js

---
+++ assets/script/myshop.js
@@ -0,0 +1,20 @@
+class myShop{
+
+    constructor(data) {
+        var myshop_id;
+        var board_title;
+        var board_id;
+        var board_desc;
+        var pcs_list;
+        var pcs_image;
+        var pcs_name;
+        var result;
+        var status;  
+    }
+
+    
+
+
+
+
+}
